cd8e2f91a70646ca97865367ca5ea035a7750296
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-13 11:16:32 +0200

    creata struttura

---
+++ js/script.js
@@ -0,0 +1,116 @@
+const icons = [
+	{
+	  name: 'apple-alt',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'ice-cream',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'fish',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'lemon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'hamburger',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'pizza-slice',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'beer',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-whiskey',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'wine-bottle',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'cocktail',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'coffee',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-martini',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'dragon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'kiwi-bird',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'frog',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'hippo',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'otter',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'horse',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+  ];
+
+  const icone=document.getElementById("cards");
+
+  icons.forEach(
+      (element)=>
+  );
