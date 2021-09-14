af64b48fb4ece07f9a221abe51486074ac18b34b
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-14 09:26:35 +0200

    aggionrato esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+// Milestone 2
+// Coloriamo le icone per tipo
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
 const icons = [
 	{
 	  name: 'apple-alt',
@@ -109,20 +116,69 @@ const icons = [
 	},
   ];
 
-const containerIcone=document.getElementById("cards");
+const containerIcone=document.getElementById("icons");
 
+const stampaCard=(array,container) =>{
+    array.forEach(
+        (element)=>{
+            const{name,family,prefix} = element;
+            container.innerHTML+=
+            `<div class="card">
+                <i class="${family} ${prefix}${name}" style="color:${colori}></i>
+                <div class="icon-name">${name}</div>
+            </div>`;
+        }
+    );
+}
+
+
+const colori={
+    "food":"gold",
+    "animals":"teal",
+    "beverage":"purple"
+}
+
+const iconColorate = icons.map(
+    (element)=>{
+        return{
+            ...element,
+            color:colori[element.category]
+        };
+    }
+);
+
+
+
+const iconeCategorie=[];
 icons.forEach(
     (element)=>{
-        const {name,family,prefix} = element;
-        containerIcone.innerHTML+=`
-        <div class="card">
-            <i class="${family} ${prefix}${name}">
-                <div class="icon-name">  ${name} </div>
-            </i>
-        </div>`
-
-
-
+        if(iconeCategorie.includes(element.category)==false){
+            iconeCategorie.push(element.category);
+        }
     }
+
+);
+
+const scegliCategoria=document.getElementById("categorie");
+
+iconeCategorie.forEach(
+    (element)=>{
+        scegliCategoria.innerHTML+=`<option value="${element}">${element}</option>`
+    }
+    
+);
+
+scegliCategoria.addEventListener("change",
+ function(){
+     const filtroIcone=iconColorate.filter(
+         (element)=>{
+             if(element.category==scegliCategoria.value || scegliCategoria.value =="" ){
+                 return true;
+             }
+             return false;
+         }
+     );
+     stampaCard(iconColorate,containerIcone);
+ }
 
 );
